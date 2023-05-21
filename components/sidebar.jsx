4f19de56ce9d76122f8bb0db74cea5db2316fa05
Ansh <anshsaxena4190@gmail.com>
2023-05-21 23:03:34 +0530

    code rearranged

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,5 +1,20 @@
-export const Dashboard = async () => {
-    return(
-        <div></div>
-    )
-}
+import Link from "next/link";
+
+export const Sidebar = async () => {
+    return (
+        <div>
+            <ul>
+                <li className={isActive("/") ? "text-blue-500" : ""}>
+                    <div className="relative inline-block w-full text-left">
+                        <Link
+                            className="hover:opacity-75 lg:pl-5 lg:pr-5"
+                            href="/"
+                        >
+                            Home
+                        </Link>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    );
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import { Sidebar } from "@/components/sidebar";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Sidebar />
+            <Component {...pageProps} />
+        </div>
+    );
 }
