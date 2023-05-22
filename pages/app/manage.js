f2da0b570c1771639339a5aa8c91b7e00f48dd66
Ansh <anshsaxena4190@gmail.com>
2023-05-23 01:34:11 +0530

    upload callback link

--- pages/app/manage.js
+++ pages/app/manage.js
@@ -1,57 +1,23 @@
-// https://anshs-gum3road.infura-ipfs.io/ipfs/QmXVW8S93KP76jwFqrJjXawyJMjnax3mr1U3dzSovfvch1
-
 import { Navbar } from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
 import { upload } from "@spheron/browser-upload";
 import { useState } from "react";
-// import { Web3Storage } from "web3.storage";
 
 export default function Dashboard() {
     
     const [inputLink, setInputLink] = useState("");
 
-    // ---------- web3.storage
-
-    // const web3StorageKey = process.env.NEXT_PUBLIC_WEB3STORAGE;
-
-    // function getAccessToken() {
-    //     return web3StorageKey;
-    // }
-
-    // function makeStorageClient() {
-    //     return new Web3Storage({ token: getAccessToken() });
-    // }
-
-    // const uploadToIPFS = async (files) => {
-    //     const client = makeStorageClient();
-    //     const cid = await client.put(files);
-    //     return cid;
-    // };
-
-    // const uploadWithWeb3Storage = async (e) => {
-    //     const data = e.target.files[0];
-    //     const files = [new File([data], "data.json")];
-    //     try {
-    //         const metaCID = await uploadToIPFS(files);
-    //         const metaUrl = `https://ipfs.io/ipfs/${metaCID}/data.json`;
-    //         console.log(metaUrl);
-    //         return metaUrl;
-    //     } catch (error) {
-    //         console.log("Error uploading:", error);
-    //     }
-    // };
-
     // ---------- spheron
 
     const uploadWithSpheron = async (e) => {
         const response = await fetch(`http://localhost:3000/api/hello`); // get the temporary access token from server
-        console.log(response);
+        // console.log(response);
         const resJson = await response.json();
-        console.log(resJson);
+        // console.log(resJson);
         const token = resJson.uploadToken;
 
         const files = e.target.files[0];
-        console.log(files);
+        // console.log(files);
 
         let currentlyUploaded = 0;
 
@@ -65,6 +31,8 @@ export default function Dashboard() {
                 },
             }
         );
+        console.log(`${protocolLink}/${files.name}`)
+        setInputLink(`${protocolLink}/${files.name}`)
     };
 
     // ----------
